f6c7c3fda0d4fdfb07b672b6a7bd94313682bcf2
0PUwU <PerezOw970@northwested.org>
2024-09-18 10:57:37 -0400

    add files

---
+++ main.py
@@ -0,0 +1,15 @@
+# Owen Perez
+# 9/18/2024
+# IPO Intro
+
+# num1 = input('Enter a number: (Example: 7)\n')
+# num2 = input('Enter another number: (Example: 4)\n')
+
+# or float()
+
+num1 = float(input('Enter a number: \n'))
+num2 = float(input('Enter another number: \n'))
+
+total = num1 + num2
+
+print(f'The sum of {num1} and {num2} is: {total}')
